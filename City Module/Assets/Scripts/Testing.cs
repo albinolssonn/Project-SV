ff48a60c2cc47f9c0b8444e2c1f0a761453005ce
NicolasPetrisi <81298242+NicolasPetrisi@users.noreply.github.com>
2021-06-14 16:57:40 +0200

    Start of git
    The beginning

---
+++ City Module/Assets/Scripts/Testing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        MyGrid grid = new MyGrid(4, 2, 10f);
+    }
+}
+
+
