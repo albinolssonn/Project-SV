d01cd980851759ae3b51295ce252958677fadc0d
albinolssonn <karlalbinolsson@gmail.com>
2021-06-22 12:08:48 +0200

    Initial Gui Push
    Fixed Skeleton For Main Menu GUI

---
+++ City Module Prototype/Assets/Scripts/GUI/MainMenuBtns.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBtns : MonoBehaviour
+{
+    public void Start_btn()
+    {
+        Debug.Log("I Clicked Start!"); 
+    }
+
+    public void Settings_btn()
+    {
+        Debug.Log("I Clicked Settings!"); 
+    }
+
+    public void Quit_btn()
+    {
+        Debug.Log("I Clicked Quit!"); 
+    }
+}
