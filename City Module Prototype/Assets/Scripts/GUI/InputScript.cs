ceb728245cbeb43bd9e60d95fa0ae28d2f3adc31
albinolssonn <karlalbinolsson@gmail.com>
2021-07-06 09:25:10 +0200

    "Discard Toggle"-function for limitedAntennas
    "Discard Toggle"-function for limitedAntennas

--- City Module Prototype/Assets/Scripts/GUI/InputScript.cs
+++ City Module Prototype/Assets/Scripts/GUI/InputScript.cs
@@ -9,25 +9,35 @@ public class InputScript : MonoBehaviour
     public InputField inputLimitedAntennas;
     public GameObject visualTextLimitedAntennas;
     private GridManager gridManager;
+    private GameObject toggle; 
 
     public void Start()
     {
         gridManager = GameObject.FindGameObjectsWithTag("Grid")[0].GetComponent<GridManager>();
+        toggle = GameObject.Find("LimitedAntennas_Toggle"); 
     }
 
     public void EnterMaxValue()
     {
-        try
+        string input = inputLimitedAntennas.GetComponentInChildren<Text>().text;
+        if (!input.Equals(""))
         {
-            gridManager.SetAndUpdateMaxAntennas(int.Parse(inputLimitedAntennas.GetComponentInChildren<Text>().text));
-            inputLimitedAntennas.gameObject.SetActive(false);
-            visualTextLimitedAntennas.SetActive(true);
-        }
-        catch (System.FormatException)
-        {
-            Debug.Log("Invalid Input");
+            try
+            {
+                gridManager.SetAndUpdateMaxAntennas(int.Parse(inputLimitedAntennas.GetComponentInChildren<Text>().text));
+                inputLimitedAntennas.gameObject.SetActive(false);
+                visualTextLimitedAntennas.SetActive(true);
+            }
+            catch (System.FormatException)
+            {
+                Debug.Log("Invalid Input");
 
+            }
+        } else
+        {
+            toggle.GetComponent<Toggle>().isOn = false;
         }
+        
 
     }
     
